be323002f86932985557aa0c652ff57f9abc75fc
Tom Gobich <tom@tomgobich.com>
2025-07-09 18:34:41 -0400

    fix: email validation

--- backend/app/validators/auth.ts
+++ backend/app/validators/auth.ts
@@ -1,24 +1,23 @@
 import vine from '@vinejs/vine'
 
-const password = vine.string().minLength(8)
-
 export const registerValidator = vine.compile(
   vine.object({
     email: vine
       .string()
       .email()
-      .normalizeEmail()
+      .toLowerCase()
+      .trim()
       .unique(async (db, value) => {
         const match = await db.from('users').select('id').where('email', value).first()
         return !match
       }),
-    password,
+    password: vine.string().minLength(8),
   })
 )
 
 export const loginValidator = vine.compile(
   vine.object({
-    email: vine.string().email().normalizeEmail(),
-    password,
+    email: vine.string().email().toLowerCase().trim(),
+    password: vine.string(),
   })
 )
